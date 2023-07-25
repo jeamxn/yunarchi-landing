63de52aebfcec1d7b686d4976d637e527ab09d23
최재민 <admin@chicken-moo.com>
2023-07-25 23:45:23 +0900

    feat: contact page

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -15,8 +15,24 @@ const Contact = () => {
       </DefaultHead>
       <main className={["main", styles.main].join(" ")}>
         <Header />
-        <div className={styles.contact}>
-          
+        <div className={styles.container}>
+          <div className={styles.address}>
+            <div className={styles.title}>Address</div>
+            <div className={styles.desc}>
+              <div className={styles.leftright}>
+                <div>부산광역시 수영구 남천1동</div>
+                <div>광남로10번길 2 동남빌딩 4,5층</div>
+              </div>
+              <div className={styles.leftright}>
+                <div>+82 051 911 9011</div>
+                <div>yun-archi16@daum.net</div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.address}>
+            <div className={styles.title}>Map</div>
+
+          </div>
         </div>
       </main>
     </>
