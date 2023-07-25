aca582f88d7ff503c0c12c90226e7e471c2b4b87
최재민 <admin@chicken-moo.com>
2023-07-26 01:26:00 +0900

    fix: design fix

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -18,12 +18,11 @@ const Contact = () => {
             <div className={styles.title}>Address</div>
             <div className={styles.desc}>
               <div className={styles.leftright}>
-                <div>부산광역시 수영구 남천1동</div>
-                <div>광남로10번길 2 동남빌딩 4,5층</div>
+                <div className={[styles.ls, styles.lss].join(" ")}>부산광역시 수영구 남천1동 광남로10번길 2 동남빌딩 4,5층</div>
               </div>
               <div className={styles.leftright}>
-                <div>+82 051 911 9011</div>
-                <div>yun-archi16@daum.net</div>
+                <div className={styles.ls}><a href="tel:0519119011">+82 051 911 9011</a></div>
+                <div className={styles.ls}><a href="mailto:yun-archi16@daum.net">yun-archi16@daum.net</a></div>
               </div>
             </div>
           </div>
