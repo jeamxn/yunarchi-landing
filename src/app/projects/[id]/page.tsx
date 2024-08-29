833debf13ea9b40c0109fc7c973ff4a3bb2e605e
jeamxn <admin@chicken-moo.com>
2024-08-29 10:49:23 +0900

    feat: re

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,63 +1,46 @@
-"use client";
-
-import axios from "axios";
-import Image from "next/image";
+import { Client } from "@notionhq/client";
+import { ImageBlockObjectResponse, PageObjectResponse } from "@notionhq/client/build/src/api-endpoints";
 import React from "react";
 
-import { Main } from "@/components";
-import styles from "@/styles/pages/Project.module.css";
+import PageContent from "./pageContent";
 
 type Props = {
-  id: number;
+  id: string;
 };
-const Page = ({ params }: {
+const Page = async ({ params }: {
   params: Props;
 }) => {
-  const [imgLoading, setImgLoading] = React.useState(false);
-  const [data, setData] = React.useState("");
-  const [images, setImages] = React.useState<string[]>([]);
-  const [i, setI] = React.useState(0);
+  if(!params.id) return;
 
-  const init = async () => {
-    setImgLoading(true);
-    const { data: res } = await axios({
-      method: "GET",
-      url: `/api/project/${params.id}`,
-    });
-    setImages(res.images);
-    setData(res.title);
+  const block_id = params.id;
+
+  const notion = new Client({
+    auth: process.env.NOTION_API_KEY,
+  });
+  
+  const [pages, blocks] = await Promise.all([
+    notion.pages.retrieve({ page_id: block_id }),
+    notion.blocks.children.list({ block_id }),
+  ]);
+
+  const images = (blocks.results as ImageBlockObjectResponse[]).map((block) => {
+    if(block.type !== "image") return;
+    if(block.image.type !== "file") return;
+    return block.image.file.url;
+  }).filter((url) => url !== undefined);
+
+  const page = () => {
+    const page = pages as PageObjectResponse;
+    const properties = page.properties;
+    if(properties.Name.type !== "title") return "";
+    const title = properties.Name.title[0].plain_text;
+    if(properties.Tags.type !== "multi_select") return "";
+    const tags = properties.Tags.multi_select.map((tag) => tag.name).join(", ");
+    return `${title} - ${tags}`;
   };
 
-  React.useEffect(() => {
-    if(!params.id) return;
-    init();
-  }, [params.id]);
-
   return (
-    <Main className={styles.container}>
-      <div className={!imgLoading ? styles.imageBox : styles.imageCover}>
-        {
-          data ? <Image
-            alt="project image"
-            src={images[i]}
-            width={630}
-            height={420}
-            className={styles.image}
-            onClick={() => setI((prv) => {
-              setImgLoading(true);
-              if(prv === images.length - 1) return 0;
-              return prv + 1;
-            })}
-            onLoad={() => setImgLoading(false)}
-            style={{
-              background: !imgLoading ? "#fff" : "",
-              opacity: !imgLoading ? "1" : "",
-            }}
-          /> : null
-        }
-      </div>
-      <div className={styles.infoName}>{data}</div>
-    </Main>
+    <PageContent res={{ title: page(), images }} />
   );
 };
 
---
+++ src/app/projects/[id]/pageContent.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import Image from "next/image";
+import React from "react";
+
+import { Main } from "@/components";
+import styles from "@/styles/pages/Project.module.css";
+
+const PageContent = ({ 
+  res,
+}: {
+  res: {
+    title: string;
+    images: string[];
+  };
+}) => {
+  const [imgLoading, setImgLoading] = React.useState(false);
+  const [i, setI] = React.useState(0);
+
+  return (
+    <Main className={styles.container}>
+      <div className={!imgLoading ? styles.imageBox : styles.imageCover}>
+        {
+          res.images ? <Image
+            alt="project image"
+            src={res.images[i]}
+            width={630}
+            height={420}
+            className={styles.image}
+            onClick={() => setI((prv) => {
+              setImgLoading(true);
+              if(prv === res.images.length - 1) return 0;
+              return prv + 1;
+            })}
+            onLoad={() => setImgLoading(false)}
+            style={{
+              background: !imgLoading ? "#fff" : "",
+              opacity: !imgLoading ? "1" : "",
+            }}
+          /> : null
+        }
+      </div>
+      <div className={styles.infoName}>{res.title}</div>
+    </Main>
+  );
+};
+
+export default PageContent;
